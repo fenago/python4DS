3741af481f442010946361b6539641f4c56495dd
fenago <socrates73@gmail.com>
2021-08-31 09:55:20 -0400

    Add files via upload

--- shapes.py
+++ shapes.py
@@ -6,7 +6,7 @@ This is an introduction to MODULES
 CHAR='*'
 def rectangle(height,width):
     '''Prints a rectangle'''
-    for row in rnage(height):
+    for row in range(height):
         for col in range(width):
             print(CHAR,end='')
         print()
@@ -15,7 +15,7 @@ def square(side):
     rectangle(side,side)
 def triangle(height):
     '''Prints a triangle'''
-    for rown in range(height):
+    for row in range(height):
         for col in range(1,row+2):
-            print(CHARE,end='')
+            print(CHAR,end='')
         print()
